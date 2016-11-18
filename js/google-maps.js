17d933dcc9e8532cfd04ee2f8d908e3e8b1cc143
Gediminas Bivainis <gediminas@bivainis.com>
2016-11-18 17:16:35 +0100

    add scrollwheel and panControl options

--- js/google-maps.js
+++ js/google-maps.js
@@ -1,14 +1,21 @@
 jQuery(document).ready(function () {
     var $mapDivs = $('.google-map');
-    $mapDivs.each(function(index){
+
+    $mapDivs.each(function (index) {
         var lat = parseFloat($(this).data('lat'));
         var lng = parseFloat($(this).data('lng'));
         var zoom = parseInt($(this).data('zoom'));
+        var scrollwheel = Boolean($(this).data('scrollwheel'));
+        var panControl = Boolean($(this).data('panControl'));
         var window = $(this).data('infowindow');
+
         var map = new google.maps.Map(this, {
             center: {lat: lat, lng: lng},
-            zoom: zoom
+            zoom: zoom,
+            scrollwheel: scrollwheel,
+            panControl: panControl
         });
+
         var marker = new google.maps.Marker({
             position: {lat: lat, lng: lng},
             map: map,
@@ -16,13 +23,12 @@ jQuery(document).ready(function () {
             title: 'Hello World!'
         });
 
-        if (window)
-        {
+        if (window) {
             var infowindow = new google.maps.InfoWindow({
                 content: window
             });
 
-            marker.addListener('click', function() {
+            marker.addListener('click', function () {
                 infowindow.open(map, marker);
             });
 
